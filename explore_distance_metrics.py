3f1af944cde25ad2f29fe8b5bf305530bcf93342
Alex Berman <alex@nimations.com>
2018-05-11 08:47:05 +0200

    explore_distance_metrics: Save data to file

--- explore_distance_metrics.py
+++ explore_distance_metrics.py
@@ -1,6 +1,11 @@
+import cPickle
 import model_analysis
 import spacy
 from prettytable import PrettyTable
+
+def save_to_file(filename, data):
+    with open(filename, "w") as f:
+        cPickle.dump(data, f)
 
 print "Loading model..."
 nlp = spacy.load("en_core_web_md", disable=["parser"])
@@ -8,28 +13,36 @@ analyzer = model_analysis.SpacyModelAnalyzer(nlp)
 
 num_words = 100
 
-lexemes_by_frequency = sorted(
-    [lexeme for lexeme in nlp.vocab if lexeme.has_vector],
-    key=lambda lexeme: lexeme.prob,
+word_ids_by_frequency = sorted(
+    [lexeme.orth_ for lexeme in nlp.vocab if lexeme.has_vector],
+    key=lambda word_id: nlp.vocab[word_id].prob,
     reverse=True)
-common_lexemes = lexemes_by_frequency[:num_words]
+common_word_ids = word_ids_by_frequency[:num_words]
 
 all_results = []
-for lexeme1 in common_lexemes:
-    print lexeme1.orth_
+for i, word_id1 in enumerate(common_word_ids):
+    lexeme1 = nlp.vocab[word_id1]
+    print "i=%d lexeme1.orth_=%r" % (i, lexeme1.orth_)
     result = analyzer.compare_distance_metrics(lexeme1.orth_)
     for lexeme2, cosine_rank, cosine_distance, euclidean_rank, euclidean_distance in result:
+        word_id2 = lexeme2.orth_
         all_results.append((
-            lexeme1, lexeme2, cosine_rank, cosine_distance, euclidean_rank, euclidean_distance))
+            word_id1, word_id2,
+            cosine_rank, cosine_distance,
+            euclidean_rank, euclidean_distance))
+
+save_to_file("data/distance_metrics_%d_all_results.cPickle" % num_words, all_results)
 
 sorted_by_rank = sorted(
     all_results,
     key=lambda result: min(result[2], result[4]))
 
+save_to_file("data/distance_metrics_%d_by_rank.cPickle" % num_words, sorted_by_rank)
+
 table = PrettyTable(["Word 1", "Word 2", "Cosine rank", "Cosine dist", "Euclidean rank", "Euclidean dist"])
-for lexeme1, lexeme2, cosine_rank, cosine_distance, euclidean_rank, euclidean_distance in sorted_by_rank:
+for word_id1, word_id2,cosine_rank, cosine_distance, euclidean_rank, euclidean_distance in sorted_by_rank:
     table.add_row([
-        lexeme1.orth_, lexeme2.orth_,
+        nlp.vocab[word_id1].orth_, nlp.vocab[word_id2].orth_,
         cosine_rank, "%.3f" % cosine_distance,
         euclidean_rank, "%.3f" % euclidean_distance])
 
